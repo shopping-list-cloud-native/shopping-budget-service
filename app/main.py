41ee305fdf01f4c152ea8f02a895c8cbbc77e275
Trelow <denis.iurciuc@stud.acs.upb.ro>
2026-05-11 06:23:58 +0300

    Add CI/CD test change

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.routes.health import router as health_router
 from app.routes.budget import router as budget_router
 
+# CI/CD test change
 app = FastAPI(title="Budget Service")
 
 app.include_router(health_router)
